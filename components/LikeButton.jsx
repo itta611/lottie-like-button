4a46961a541d52a811ef9f706eaced4b83aa1de1
Itta Funahashi <itta@ittanoMacBook-Pro.local>
2022-05-05 18:54:40 +0900

    create likebutton component

--- components/LikeButton.jsx
+++ components/LikeButton.jsx
@@ -1,17 +1,31 @@
 import { useEffect, useRef, useState } from 'react';
 import lottie from 'lottie-web';
+import styles from '../styles/LikeButton.module.css';
+
+let animation;
+function handleLikeButtonClick(animation) {
+  animation.play();
+}
 
 function LikeButton() {
   const ref = useRef(null);
   useEffect(() => {
-    lottie.loadAnimation({
+    animation = lottie.loadAnimation({
+      autoplay: false,
+      loop: false,
       container: ref.current,
       renderer: 'svg',
       path: '/lottie/like-button.json',
     });
     return () => animation.destroy();
   }, []);
-  return <div ref={ref} />;
+  return (
+    <div
+      ref={ref}
+      onClick={() => handleLikeButtonClick(animation)}
+      className={styles.LikeButton}
+    />
+  );
 }
 
 export default LikeButton;
